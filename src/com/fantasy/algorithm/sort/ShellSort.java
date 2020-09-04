50c699abff7cc3c67a282a05da7d3e70936fa24f
Fantasy <i_am_fantasy@163.com>
2020-09-04 09:34:48 +0800

    修改希尔排序的实现

--- src/com/fantasy/algorithm/sort/ShellSort.java
+++ src/com/fantasy/algorithm/sort/ShellSort.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
  *
  * <pre>
  *     author  : Fantasy
- *     version : 1.0, 2020-08-31
+ *     version : 1.1, 2020-09-04
  *     since   : 1.0, 2020-08-31
  * </pre>
  */
@@ -34,14 +34,12 @@ public class ShellSort {
         int length = arr.length;
         int value; // 待插入的值
         int step = length / 2; // 记录每一次跳跃式比较的增量
+        int j;
         while (step >= 1) {
             for (int i = step; i < length; i += step) {
                 value = arr[i];
-                int j = i;
-                // 注意点，与插入排序不同的是这里要">="，因为step最小为1
-                while (j >= step && arr[j - step] > value) {
+                for (j = i; j >= step && arr[j - step] > value; j -= step) {
                     arr[j] = arr[j - step];
-                    j -= step;
                 }
                 arr[j] = value;
             }
